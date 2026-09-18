a32a940aca329f9724fd488499c77477ba45c96f
Quannv05022003 <2k3sinh5223@gmail.com>
2026-09-18 18:40:22 +0700

    release: v1.2.2 - C11 Kho Vat Chung

---
+++ appConfig.js
@@ -0,0 +1,8 @@
+// C?u hình các bi?n môi tru?ng cho Frontend
+window.appConfig = {
+  // Ðu?ng d?n t?i API backend
+  API_URL: "http://192.168.9.136:8088",
+  SSO_LOGIN_URL: "http://local.amp.c11.bca.vn",
+  APP_ID: "qlkvc",
+  CALLBACK_URL: "http://192.168.9.136:8088/api/auth/callback",
+};
